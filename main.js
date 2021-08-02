75e3de572386bbdc21fa097b04c5ad9d0ebe2308
neer123-ops <73882872+neer123-ops@users.noreply.github.com>
2021-08-02 14:39:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canavs.center();
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot(){
+    save('mustachioimagario.png')
+}
